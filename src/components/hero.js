506720df8ff77b30eb12fbf2fb60e5c3dc7a0a2d
Seth Lachman <lachmanseth17@gmail.com>
2020-03-08 16:38:33 -0400

    Updated styles for hero section and testimonials.

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,18 +1,24 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import React from "react"
-import { AspectRatio, Image, Heading } from "theme-ui"
+import { Box, Heading } from "theme-ui"
 
 export default ({ src, title }) => {
   return (
-    <>
-      <AspectRatio ratio={16 / 9}>
-        <Image
-          src={src}
-          sx={{
-            objectFit: "cover",
-          }}
-        />
-        <Heading>{title}</Heading>
-      </AspectRatio>
-    </>
+    <Box
+      sx={{
+        backgroundImage: `url(${src})`,
+        backgroundSize: "cover",
+        height: "35vh",
+        backgroundPosition: "center center",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Heading as="h2" color="white">
+        {title}
+      </Heading>
+    </Box>
   )
 }
--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,16 +1,37 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import React from "react"
+import { Grid, Heading, Text, Box } from "theme-ui"
 
 export default ({ testimonials }) => {
   return (
-    <div>
-      {testimonials.map(({ node }) => {
-        return (
-          <div>
-            <h2>{node.name}</h2>
-            <p>"{node.quote}"</p>
-          </div>
-        )
-      })}
-    </div>
+    <section
+      sx={{
+        display: "flex",
+        maxWidth: "1200px",
+        margin: "2rem auto",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Heading as="h2">Testimonials</Heading>
+      <Grid my="2rem" gap={"12px 32px"} columns={["auto", "repeat(2, 1fr)"]}>
+        {testimonials.map(({ node }) => {
+          return (
+            <Box
+              sx={{
+                boxShadow: "0 0 8px rgba(0, 0, 0, 0.125)",
+                padding: 2,
+                borderRadius: 4,
+              }}
+            >
+              <Heading as="h3">{node.name}</Heading>
+              <Text as="p">"{node.quote}"</Text>
+            </Box>
+          )
+        })}
+      </Grid>
+    </section>
   )
 }
